da5bdad2e6af51ff57bbe546b4196936e77c7a74
jacob <jacobpaul75608@gmail.com>
2024-05-16 12:25:07 +0530

    hi

---
+++ src/Models/user.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn({
+    type: 'bigint',
+    name: 'user_id',
+  })
+  id: number;
+
+  @Column({
+    name: 'Phone_Number',
+    nullable: false,
+  })
+  phNumber: number;
+}
---
+++ src/users/users.controller.ts
@@ -0,0 +1,33 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { CreateUserDto } from 'src/Models/Dtos/user.dto';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
+  @Get()
+  getUsers() {
+    return this.userService.getUsers();
+  }
+
+  @Get('email/:phoneNumber')
+  getUserByEmail(@Param('phNumber') phNumber: number) {
+    return this.userService.getUserByPhNumber(phNumber);
+  }
+
+  @Post('create')
+  @UsePipes(ValidationPipe)
+  createUsers(@Body() createUserDto: CreateUserDto) {
+    return this.userService.createUser(createUserDto);
+  }
+}
